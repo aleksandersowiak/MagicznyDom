95730d139c6eab063b4300c27ec1a55d90cc8d9b
Aleksander Sowiak <aleksander.sowiak@codeconcept.pl>
2014-12-16 14:55:28 +0100

    Change comments view

--- application/views/helpers/RestrictText.php
+++ application/views/helpers/RestrictText.php
@@ -4,7 +4,7 @@ class Zend_View_Helper_RestrictText extends Zend_View_Helper_Abstract
     public function restrictText($text, $count)
     {
         $countText = strlen($text);
-        if ($countText >= $count) {
+        if ($countText > $count) {
             $cut = substr(strip_tags($text), 0, $count);
             $restrict = ($cut) . "...";
         } else {
