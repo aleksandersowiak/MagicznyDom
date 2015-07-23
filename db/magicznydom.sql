4724cce499b6c9904622c7d796b94786baa1240d
Aleksander Sowiak <aleksander.sowiak@codeconcept.pl>
2015-07-23 13:01:43 +0200

    for all mysql server

--- db/magicznydom.sql
+++ db/magicznydom.sql
@@ -1,14 +1,6 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Wersja serwera:               5.5.21-log - MySQL Community Server (GPL)
--- Serwer OS:                    Win32
--- HeidiSQL Wersja:              9.1.0.4867
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+SET autocommit=0;
+SET foreign_key_checks = 0;
+START TRANSACTION;
 
 -- Zrzut struktury tabela magicznydom.comments
 CREATE TABLE IF NOT EXISTS `comments` (
@@ -351,3 +343,6 @@ INSERT INTO `user` (`id`, `name`, `given_name`, `family_name`, `link`, `picture`
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+COMMIT;
+SET foreign_key_checks = 1;
+SET autocommit=1;
